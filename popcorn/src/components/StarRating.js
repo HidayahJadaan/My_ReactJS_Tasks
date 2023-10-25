d7b88dc1db63fe8307a90b6bbc871eac649ae801
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-10-25 14:53:30 +0300

    using TypeScript- proptypes checking

--- popcorn/src/components/StarRating.js
+++ popcorn/src/components/StarRating.js
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -11,15 +20,6 @@ const starContainerStyle = {
   display: "flex",
 };
 
-StarRating.propTypes = {
-  maxRating: PropTypes.number,
-  defaultRating: PropTypes.number,
-  color: PropTypes.string,
-  size: PropTypes.number,
-  messages: PropTypes.array,
-  className: PropTypes.string,
-  onSetRating: PropTypes.func,
-};
 
 export default function StarRating({
   maxRating = 5,
