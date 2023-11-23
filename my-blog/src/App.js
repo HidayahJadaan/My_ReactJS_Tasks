a3b68bdaced997b38e0384ef489ececd55580ecc
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-11-23 19:35:29 +0300

    Lesson 6

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home"
 
 function App() {
- 
-}
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const person = { name: "Hidayah", age: 30 };
 
+  return (
+    <div className="app">
+        <Navbar />
+      <div className="content">
+       <Home />
+      </div>
+    </div>
+  );
+}
 export default App;
