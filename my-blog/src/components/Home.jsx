75c44e97327e1ce60317da93a977a3eeb22d7da8
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-11-24 00:22:01 +0300

    Lesson 18

--- my-blog/src/components/Home.jsx
+++ my-blog/src/components/Home.jsx
@@ -1,29 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  // let name = 'Hidayah';
-  const [name, setName] = useState("Hidayah");
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-  const handleClick = () => {
-    setName("Jadaan");
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+      })
+    }, 1000);
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} />
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      {blogs && <BlogList blogs={blogs} title="Hidayah's Blogs!" />}
+
+    
     </div>
   );
-};
-
+}
+ 
 export default Home;
