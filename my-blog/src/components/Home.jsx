6fb496f57607936455839ef3cc2743033a966019
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-11-24 15:29:07 +0300

    Lesson 20 - Custom Hooks

--- my-blog/src/components/Home.jsx
+++ my-blog/src/components/Home.jsx
@@ -1,28 +1,17 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "../useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-      })
-    }, 1000);
-  }, [])
+  const { error, isPending, data } =useFetch('http://localhost:8000/blogs');
 
+  
   return (
     <div className="home">
-      { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-      {blogs && <BlogList blogs={blogs} title="Hidayah's Blogs!" />}
+      { isPending && <div>{error}</div> }
+      {data && <BlogList blogs={data} title="All Blogs!" />}
+      {data && <BlogList blogs={data} title="Hidayah's Blogs!" />}
 
     
     </div>
