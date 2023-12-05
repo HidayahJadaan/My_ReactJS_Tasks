313b60e1f2e4cd709f2294ce721c065333c6c696
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-05 23:40:32 +0300

    Linking Between Routes

--- 15-world-wise-using-vite/src/pages/HomePage.jsx
+++ 15-world-wise-using-vite/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 
 export default function HomePage() {
   return (
     <div>
+
+      <PageNav />
       <h1>HomePage</h1>
 
 {/* We don't need this because it make the page re-loaded */}
