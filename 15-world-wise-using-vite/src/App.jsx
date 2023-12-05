c25854a875eaf413377d22fb1ff6ecbda674fdb2
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-05 23:01:32 +0300

    Implementing Main Pages and Routes

--- 15-world-wise-using-vite/src/App.jsx
+++ 15-world-wise-using-vite/src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing"
+import HomePage from "./pages/HomePage"
+import PageNotFound from "./pages/PageNotFound";
 
-export default function App() {
+ function App() {
   return (
-    <div>World Wise</div>
-  )
+   <div>
+{/* This will always be on the page
+<h1>Hello Router!!</h1> */}
+
+
+<BrowserRouter>
+    <Routes>
+
+      <Route path="" element={<HomePage />} />
+      <Route path="product" element={<Product/>} />
+      <Route path="pricing" element={<Pricing /> } />
+      <Route path="*" element={<PageNotFound /> } />
+
+    </Routes>
+    </BrowserRouter>
+   </div>
+
+  );
 }
+export default App;
